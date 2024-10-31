883f92fb47382610b42cfb7af8f559a605471c87
mckenziephagen <mphagen@uw.edu>
2024-10-30 22:05:36 -0700

    add functions to main.py file

---
+++ main.py
@@ -0,0 +1,38 @@
+import datalad.api as dl
+import nibabel as nib
+
+import nilearn
+
+import os
+import os.path as op
+
+from nilearn.input_data import NiftiMasker, NiftiLabelsMasker
+
+
+#create pseudo-bids naming mapping dict
+#assuming that the RL was always run before LR
+bids_dict = {
+    'rfMRI_REST1_RL': 'ses-1_run-1',
+    'rfMRI_REST2_RL': 'ses-2_run-1',
+    'rfMRI_REST2_LR': 'ses-2_run-2',
+    'rfMRI_REST1_LR': 'ses-1_run-2' 
+}
+
+def parcellate_data(file, dataset_path, masker): 
+    
+    try: 
+        ses_string = bids_dict[file.split('/')[-2]]
+    except KeyError: 
+        return
+        
+    dl.get(file, dataset=dataset_path)
+    
+    data = nib.load(file)
+    
+    time_series = masker.fit_transform(data)
+    
+    dl.drop(file, dataset=dataset_path)
+    
+    
+    return time_series, ses_string
+   
